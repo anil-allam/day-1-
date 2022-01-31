b2610e1f873aefbef5617fe29be006a778952650
unknown <anilallam01@gmail.com>
2022-01-31 18:36:24 +0530

    age

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            Console.WriteLine("enter the age:");
+            age = int.Parse(Console.ReadLine());
+            if(age>=18)
+            {
+                Console.WriteLine("eligible for vote");
+            }
+            else
+            {
+                Console.WriteLine("not eligible for vote");
+            }
+        }
+    }
+}
