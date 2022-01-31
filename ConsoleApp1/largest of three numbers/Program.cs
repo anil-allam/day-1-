7f0e75570b10426eefb2e9a3217bc4a21dbe3cc8
unknown <anilallam01@gmail.com>
2022-01-31 19:31:10 +0530

    first commit

---
+++ ConsoleApp1/largest of three numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace largest_of_three_numbers
+{
+    class Program
+    {
+        static void Main(string[] args) { 
+                Console.WriteLine("Enter number 1:");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter number 2:");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter number 3:");
+                int num3 = Convert.ToInt32(Console.ReadLine());
+                if (num1 > num2)
+                {
+                    if (num1 > num3)
+                    {
+                        Console.Write("{0} is the largest!\n", num1);
+                    }
+                    else
+                    {
+                        Console.Write("{0} is the largest!\n", num3);
+                    }
+                }
+                else if (num2 > num3)
+                    Console.Write("{0} is the largest!\n", num2);
+                else
+                    Console.Write("(0) is the largest!\n", num3);
+            }
+    }
+}
