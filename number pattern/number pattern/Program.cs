6e7ab1f050202b75940da3082c91640281c4d11f
unknown <anilallam01@gmail.com>
2022-02-01 10:41:31 +0530

    number

---
+++ number pattern/number pattern/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace number_pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, rows, k = 1;
+
+            Console.Write("Input number of rows : ");
+            rows = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= rows; i++)
+            {
+                for (j = 1; j <= i; j++)
+                    Console.Write("{0} ", k++);
+                Console.Write("\n");
+            }
+        }
+    }
+}
+    
+
