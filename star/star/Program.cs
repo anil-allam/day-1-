1a053ebbb9267fb7c9392921174a259e51e5eb46
unknown <anilallam01@gmail.com>
2022-02-01 10:29:36 +0530

    star

---
+++ star/star/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace star
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int row = 1; row <= 3; ++row)
+            {
+                for (int col = 1; col <= row; ++col)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
