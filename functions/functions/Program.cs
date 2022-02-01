f6db2ccdc46ab5ae2e1ef4b08fd38f0880eeaf26
unknown <anilallam01@gmail.com>
2022-02-01 11:14:50 +0530

    number

---
+++ functions/functions/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter no whose factorial is to be calculated");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int i, p = 1;
+            for (i = 1; i < n + 1; i++)
+            {
+                p = p * i;
+            }
+            Console.WriteLine("Factorial is " + p);
+        }
+    }
+}
